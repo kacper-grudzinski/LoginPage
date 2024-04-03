938be0644f804a1e191ade876fbaf7c5594d341f
Kacper Grudziński <kgrvdzin@gmail.com>
2024-04-03 22:50:43 +0200

    Add media queries

--- src/script.js
+++ src/script.js
@@ -4,6 +4,8 @@ const loginLink = document.querySelector('.loginLink');
 const registerLink = document.querySelector('.registerLink');
 const popup = document.querySelector('.btnLogin-popup');
 const closeIcon = document.querySelector('.iconClose');
+const hamburger = document.querySelector('.hamburger');
+const navigation = document.querySelector('.navigation');
 
 registerLink.addEventListener('click', ()=>{
     wrapper.classList.add('active');
@@ -21,3 +23,7 @@ closeIcon.addEventListener('click', ()=>{
     wrapper.classList.remove('active-popup');
     container.classList.remove('closed');
 });
+
+hamburger.addEventListener('click', ()=>{
+    navigation.classList.toggle('open');
+})
