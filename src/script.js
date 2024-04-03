16127e73e11b5c0c7c21b86cc45e36548768ffc3
Kacper Grudziński <kgrvdzin@gmail.com>
2024-04-03 19:30:02 +0200

    Add js & welcome content

--- src/script.js
+++ src/script.js
@@ -1,6 +1,9 @@
 const wrapper = document.querySelector('.wrapper');
+const container = document.querySelector('.container');
 const loginLink = document.querySelector('.loginLink');
 const registerLink = document.querySelector('.registerLink');
+const popup = document.querySelector('.btnLogin-popup');
+const closeIcon = document.querySelector('.iconClose');
 
 registerLink.addEventListener('click', ()=>{
     wrapper.classList.add('active');
@@ -8,4 +11,13 @@ registerLink.addEventListener('click', ()=>{
 
 loginLink.addEventListener('click', ()=>{
     wrapper.classList.remove('active');
+});
+
+popup.addEventListener('click', ()=>{
+    wrapper.classList.add('active-popup');
+    container.classList.add('closed');
+});
+closeIcon.addEventListener('click', ()=>{
+    wrapper.classList.remove('active-popup');
+    container.classList.remove('closed');
 });
